fd31b136fce6ee75e79313856aaf6cdd5abecbe3
yiguanxianyu <xianyu@pku.edu.cn>
2022-03-15 11:26:27 +0800

    Common update

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from zipfile import ZipFile
 
 from pymobiledevice3.usbmux import list_devices
 from pymobiledevice3.lockdown import LockdownClient
-from pymobiledevice3.services.mobile_image_mounter import MobileImageMounterService
 from pymobiledevice3.services.simulate_location import DtSimulateLocation
+from pymobiledevice3.services.mobile_image_mounter import MobileImageMounterService
 
 
 def download_image(ios_version):
@@ -33,32 +33,32 @@ def download_image(ios_version):
 
 def play_gpx(lockdown, filename):
     print("开始模拟跑步")
-    location_simulator = DtSimulateLocation(lockdown)
+    location_simulator = DtSimulateLocation(lockdown=lockdown)
     location_simulator.play_gpx_file(filename)
     location_simulator.clear()
     print("跑步完成，请在手机中结束跑步")
 
 
 def unmount_image(lockdown):
-    image_type = 'Developer'
-    mount_path = '/Developer'
+    image_type = "Developer"
+    mount_path = "/Developer"
     image_mounter = MobileImageMounterService(lockdown=lockdown)
-    image_mounter.umount(image_type, mount_path, b'')
-    print('成功卸载镜像')
+    image_mounter.umount(image_type, mount_path, b"")
+    print("成功卸载镜像")
 
 
 def mount_image(lockdown, version):
-    image_type = 'Developer'
+    image_type = "Developer"
 
     image_mounter = MobileImageMounterService(lockdown=lockdown)
     if image_mounter.is_image_mounted(image_type):
-        print('镜像已经被挂载过了')
+        print("镜像已经被挂载过了")
         return
 
     download_image(version)
 
-    image = f'./Contents/Developer/Platforms/iPhoneOS.platform/DeviceSupport/{version}/DeveloperDiskImage.dmg'
-    signature = image + '.signature'
+    image = f"./Contents/Developer/Platforms/iPhoneOS.platform/DeviceSupport/{version}/DeveloperDiskImage.dmg"
+    signature = image + ".signature"
 
     image = Path(image)
     image = image.read_bytes()
@@ -69,15 +69,16 @@ def mount_image(lockdown, version):
     image_mounter.upload_image(image_type, image, signature)
     image_mounter.mount(image_type, signature)
 
-    print('镜像挂载成功')
+    print("镜像挂载成功")
 
 
-def main():
+def main(gpx_path):
     device_list = list_devices()
     if not device_list:
         print("未查找到设备，请确认：")
         print("  1. 是否已下载并安装 iTunes")
         print("  2. 是否已将设备连接到电脑、解锁设备并勾选信任选项")
+        input("按下回车退出")
         return
 
     device = device_list[0]
@@ -98,13 +99,20 @@ def main():
         ios_version = ios_version_replace[ios_version]
 
     mount_image(device_LockdownClient, ios_version)
-    play_gpx(device_LockdownClient, "./samples/route_2022-02-26_9.00pm.gpx")
+    play_gpx(device_LockdownClient, gpx_path)
     unmount_image(device_LockdownClient)
     input("按下回车键退出")
 
 
 if __name__ == "__main__":
-    print("------------------")
-    print("|   PKUAutoRun   |")
-    print("------------------")
-    main()
+    print(r"""
+  _____  _  ___    _              _        _____
+ |  __ \| |/ / |  | |  /\        | |      |  __ \
+ | |__) | ' /| |  | | /  \  _   _| |_ ___ | |__) |   _ ____
+ |  ___/|  < | |  | |/ /\ \| | | | __/ _ \|  _  / | | |  _  \
+ | |    | . \| |__| / ____ \ |_| | || (_) | | \ \ |_| | | | |
+ |_|    |_|\_\_____/_/    \_\____|\__\___/|_|  \_\____|_| |_|
+
+""")
+
+    main("./samples/route_2022-02-26_9.00pm.gpx")
