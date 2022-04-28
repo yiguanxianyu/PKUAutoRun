10e9de8fd194453a5a67fe6b532ce53d5593811f
yiguanxianyu <xianyu@pku.edu.cn>
2022-04-28 23:46:20 +0800

    Add multi-device support

--- image.py
+++ image.py
@@ -5,48 +5,48 @@ from pymobiledevice3.services.mobile_image_mounter import MobileImageMounterServ
 
 
 def download_image(ios_version):
-    base_dir = "./Contents/Developer/Platforms/iPhoneOS.platform/DeviceSupport/"
-    base_url = "https://raw.fastgit.org/filsv/iPhoneOSDeviceSupport/master/"
+    base_dir = './Contents/Developer/Platforms/iPhoneOS.platform/DeviceSupport/'
+    base_url = 'https://raw.fastgit.org/filsv/iPhoneOSDeviceSupport/master/'
 
-    img_dir = base_dir + ios_version + ".zip"
-    img_url = base_url + ios_version + ".zip"
+    img_dir = base_dir + ios_version + '.zip'
+    img_url = base_url + ios_version + '.zip'
 
     Path(base_dir).mkdir(parents=True, exist_ok=True)
 
-    print("开始下载模拟所需的开发者镜像文件")
+    print('开始下载模拟所需的开发者镜像文件')
     if not Path(img_dir).exists():
-        print("···正在下载镜像文件，此过程可能较费时，请耐心等待")
+        print('···正在下载镜像文件，此过程可能较费时，请耐心等待')
         download(img_url, base_dir)
         print()
     else:
-        print("···镜像文件已存在")
-    print("镜像下载完成")
+        print('···镜像文件已存在')
+    print('镜像下载完成')
 
     with ZipFile(img_dir) as zf:
         zf.extractall(base_dir)
-        print("镜像解压缩完成")
+        print('镜像解压缩完成')
 
 
 def unmount_image(lockdown):
-    image_type = "Developer"
-    mount_path = "/Developer"
+    image_type = 'Developer'
+    mount_path = '/Developer'
     image_mounter = MobileImageMounterService(lockdown=lockdown)
-    image_mounter.umount(image_type, mount_path, b"")
-    print("成功卸载镜像")
+    image_mounter.umount(image_type, mount_path, b'')
+    print('成功卸载镜像')
 
 
 def mount_image(lockdown, version):
-    image_type = "Developer"
+    image_type = 'Developer'
 
     image_mounter = MobileImageMounterService(lockdown=lockdown)
     if image_mounter.is_image_mounted(image_type):
-        print("镜像已经被挂载过了")
+        print('镜像已经被挂载过了')
         return
 
     download_image(version)
 
-    image = f"./Contents/Developer/Platforms/iPhoneOS.platform/DeviceSupport/{version}/DeveloperDiskImage.dmg"
-    signature = image + ".signature"
+    image = f'./Contents/Developer/Platforms/iPhoneOS.platform/DeviceSupport/{version}/DeveloperDiskImage.dmg'
+    signature = image + '.signature'
 
     image = Path(image)
     image = image.read_bytes()
@@ -57,4 +57,4 @@ def mount_image(lockdown, version):
     image_mounter.upload_image(image_type, image, signature)
     image_mounter.mount(image_type, signature)
 
-    print("镜像挂载成功")
+    print('镜像挂载成功')
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from itertools import product
 import image
 import asyncio
 from preprocessing import PreProcessor
@@ -18,7 +19,7 @@ def auto_run(lockdown, points):
     start_time = points[0].time
     loop = asyncio.get_event_loop()
 
-    print("开始模拟跑步")
+    print('开始模拟跑步')
     loop.run_until_complete(
         asyncio.gather(*(
             set_loc(location_simulator,
@@ -35,23 +36,36 @@ def auto_run(lockdown, points):
 def main(gpx_path, run_speed):
     device_list = list_devices()
     if not device_list:
-        print("未查找到设备，请确认：")
-        print("  1. 是否已下载并安装 iTunes")
-        print("  2. 是否已将设备连接到电脑、解锁设备并勾选信任选项")
-        input("按下回车退出")
+        print('未查找到设备，请确认：')
+        print('  1. 是否已下载并安装 iTunes')
+        print('  2. 是否已将设备连接到电脑、解锁设备并勾选信任选项')
+        input('按下回车退出')
         return
 
-    device = device_list[0]
+    num_devices = len(device_list)
+    if num_devices > 1:
+        print('发现多台设备:')
+
+        for i in range(num_devices):
+            print(i, LockdownClient(device_list[i].serial).all_values['DeviceName'])
+
+        index = int(input('请输入对应数字以选择设备: '))
+        device = device_list[index]
+
+    else:
+        device = device_list[0]
+
     device_lockdown_client = LockdownClient(device.serial)
     device_info = device_lockdown_client.all_values
 
-    ios_version = device_info["ProductVersion"][0:4]
-    device_name = device_info["DeviceName"]
+    device_class = device_info['DeviceClass']
+    ios_version = device_info['ProductVersion'][0:4]
+    device_name = device_info['DeviceName']
 
-    print(f"已连接到您的设备：{device_name}, iOS/iPadOS 版本为{ios_version}。")
-    print("请确保在镜像挂载完成之前手机已解锁且屏幕亮起。")
+    print(f'已连接到您的 {device_class}: {device_name}, iOS/iPadOS 版本为 {ios_version}。')
+    print('请确保在镜像挂载完成之前手机已解锁且屏幕亮起。')
 
-    ios_version_replace = {"14.8": "14.7", "15.1": "15.0"}
+    ios_version_replace = {'14.8': '14.7', '15.1': '15.0'}
     if ios_version in ios_version_replace:
         ios_version = ios_version_replace[ios_version]
 
@@ -63,13 +77,13 @@ def main(gpx_path, run_speed):
 
     image.unmount_image(device_lockdown_client)
 
-    arg = input("跑步完成, 请在手机上结束跑步, 然后重启设备, 输入1将自动重启")
-    if arg == "1":
+    arg = input('跑步完成, 请在手机上结束跑步, 然后重启设备, 输入1将自动重启')
+    if arg == '1':
         DiagnosticsService(device_lockdown_client).restart()
 
 
-if __name__ == "__main__":
-    print(r"""
+if __name__ == '__main__':
+    print(r'''
   _____  _  ___    _              _        _____
  |  __ \| |/ / |  | |  /\        | |      |  __ \
  | |__) | ' /| |  | | /  \  _   _| |_ ___ | |__) |   _ ____
@@ -77,12 +91,13 @@ if __name__ == "__main__":
  | |    | . \| |__| / ____ \ |_| | || (_) | | \ \ |_| | | | |
  |_|    |_|\_\_____/_/    \_\____|\__\___/|_|  \_\____|_| |_|
 
-""")
+提示: 如果要使用多台设备同时打开, 请多开本程序
+''')
 
-    path = input("请输入 GPX 文件路径或将文件拖入到这里：")
-    speed = int(input("请输入你希望的平均配速(秒), 如五分配速输入300:"))
+    path = input('请输入 GPX 文件路径或将文件拖入到这里：')
+    speed = int(input('请输入你希望的平均配速(秒), 如五分配速输入300:'))
 
     # 如果在 Python 脚本中运行程序，可以直接在下面修改路径使用：
-    # path = r"./samples/54_6.5km.gpx"
+    # path = r'./samples/54_6.5km.gpx'
     # speed = 280
     main(path, speed)
